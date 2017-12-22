dcc0ed26b465fa6e523decb7d02a740981aae0ac
FeiXCMeng <Weiyu.DWWC@Gmail.com>
2017-12-22 18:04:29 +0800

    unittest

---
+++ src/utils/unittest/logging.py
@@ -0,0 +1,12 @@
+# # -*- coding: utf-8 -*-
+# import logging
+
+# logger = logging.getLogger('test')
+# logging.baseConfig()
+# logger.setLevel(logging.DEBUG)
+
+# logger.info('info message')
+# logger.error('error message')
+
+list1 = [i for i in range(1, 5)]
+print(list1)
